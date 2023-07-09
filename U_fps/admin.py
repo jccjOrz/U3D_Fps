1e891afb14fb91bdd5287a824103a47987f38ed5
jc <2425796900@qq.com>
2023-07-09 12:23:48 +0800

    后端代码

---
+++ U_fps/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from U_fps.models import Room
+
+admin.site.register(Room)
---
+++ U_fps/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from U_fps.views import index,get_room_list,build_room,remove_room
+
+
+urlpatterns=[
+        path("",index),
+        path("get_room_list/", get_room_list),
+        path("build_room/", build_room),
+        path("remove_room/", remove_room),
+
+        ]
